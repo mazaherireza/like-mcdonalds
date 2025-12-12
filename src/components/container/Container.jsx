c8bbaa05e431189c4e12bd45b556434cd88e0de3
mazaherireza <rezamazaheri.email@gmail.com>
2025-12-12 11:06:25 +0330

    Delete Unused File

--- src/components/container/Container.jsx
+++
@@ -1,8 +0,0 @@
-import routes from "../../routes";
-import { useRoutes } from "react-router";
-
-export default function Container() {
-  const router = useRoutes(routes);
-
-  return <>{router}</>;
-}
