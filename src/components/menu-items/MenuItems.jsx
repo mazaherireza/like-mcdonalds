48e398fd2eca398812cb09f72a980ae00b807a38
mazaherireza <rezamazaheri.email@gmail.com>
2025-12-13 08:45:38 +0330

    Fix Some Issues on Mobile Menu

--- src/components/menu-items/MenuItems.jsx
+++ src/components/menu-items/MenuItems.jsx
@@ -6,20 +6,24 @@ import { categories } from "@/components/Categories";
 
 import "./MenuItems.css";
 
-export default function MenuItems() {
+export default function MenuItems({ hideMenu }) {
   const { setShouldChange } = useDropdownContext();
+
+  const handleClick = () => {
+    if (hideMenu) {
+      hideMenu();
+    } else {
+      setShouldChange((prev) => !prev);
+    }
+  };
 
   return (
     <>
       {categories.map((category) => {
         const { _id, img, title } = category;
-        
+
         return (
-          <li
-            className="menu-items"
-            key={_id}
-            onClick={() => setShouldChange((prev) => !prev)}
-          >
+          <li className="menu-items" key={_id} onClick={handleClick}>
             <NavLink to={`full-menu/${_id}`}>
               <div className="img-wrapper">
                 <img src={img} />
